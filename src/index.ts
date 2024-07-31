eb1c305929b73b3bd40d71e9a52e5fd061aa4476
Bagus Dermawan Mulya <bagusdermawanmulya27@gmail.com>
2024-07-31 13:31:19 +0700

    Add product management endpoints

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,6 @@ import express, { Request, Response } from "express";
 const app = express();
 const port = 3000;
 
-// Endpoint untuk register user
-app.post("/register", (req: Request, res: Response) => {
-  const { username, password } = req.body;
-  // Logika untuk register user
-  res.send("User registered");
-});
-
-// Endpoint untuk login user
-app.post("/login", (req: Request, res: Response) => {
-  const { username, password } = req.body;
-  // Logika untuk login user
-  res.send("User logged in");
-});
-
 // Endpoint untuk menambah produk
 app.post("/products", (req: Request, res: Response) => {
   const { name, price } = req.body;
